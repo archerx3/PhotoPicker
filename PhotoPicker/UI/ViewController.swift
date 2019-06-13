535a4bfcce65e6bd5efd4405839e492ae502c42d
Archer.Chen <archer.chen@axel.org>
2019-06-13 11:41:07 +0800

    Init code

---
+++ PhotoPicker/UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoPicker1
+//
+//  Created by archer.chen on 6/13/19.
+//  Copyright © 2019 CA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
